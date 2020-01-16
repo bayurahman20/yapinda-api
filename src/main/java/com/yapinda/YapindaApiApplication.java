ee21a1535608355e6a5297271ddd85f933b81a7b
bayu.r.a.20 <bayurahman497@gmail.com>
2020-01-16 22:40:32 +0700

    initial commit

---
+++ src/main/java/com/yapinda/YapindaApiApplication.java
@@ -0,0 +1,13 @@
+package com.yapinda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YapindaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YapindaApiApplication.class, args);
+    }
+
+}
